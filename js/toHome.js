e8eab52693397237aa4e0071a68079713ef99716
Ghicks <123009878+GavibH@users.noreply.github.com>
2024-12-17 07:58:02 -0500

    Add files via upload

---
+++ js/toHome.js
@@ -0,0 +1,4 @@
+function toHome() {
+    window.location.href = "file:///Users/g.hicks26/Desktop/CS%20Club/hub/main.html?a=0";
+    document.getElementById("open-animation").style.display = "none";
+}
